630e89a7f046de254aca6387fddcec9b37459aa3
Md.Asikul Islam <asik.ruet@gmail.com>
2019-07-15 23:18:44 -0700

    Add files via upload

---
+++ Employee_Management crud entity framework/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+
+
+        String connectionString = @"Data Source=(localdb)\v11.0;Initial Catalog=Sample;Integrated Security=True";
+
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+
+            DataTable dtblProduct = new DataTable();
+
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("Select * from tblEmployee", sqlcon);
+                sqlDa.Fill(dtblProduct);
+
+
+            }
+            return View(dtblProduct);
+        }
+    }
+}
+
+
+
